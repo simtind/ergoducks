548876122a29a5ebfeea62c4055290816dbb19bb
Simen August Tinderholt <simtind@gmail.com>
2026-01-01 00:01:12 +0100

    Update with norwegian keymap

---
+++ boards/shields/ergoducks/keys_norwegian.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#pragma once
+
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/keys.h>
+
+// Aliases
+#define NO_PIPE GRAVE   // |
+#define NO_1    N1      // 1
+#define NO_2    N2      // 2
+#define NO_3    N3      // 3
+#define NO_4    N4      // 4
+#define NO_5    N5      // 5
+#define NO_6    N6      // 6
+#define NO_7    N7      // 7
+#define NO_8    N8      // 8
+#define NO_9    N9      // 9
+#define NO_0    N0      // 0
+#define NO_PLUS MINUS   // +
+#define NO_BSLS EQUAL   // (backslash)
+#define NO_Q    Q       // Q
+#define NO_W    W       // W
+#define NO_E    E       // E
+#define NO_R    R       // R
+#define NO_T    T       // T
+#define NO_Y    Y       // Y
+#define NO_U    U       // U
+#define NO_I    I       // I
+#define NO_O    O       // O
+#define NO_P    P       // P
+#define NO_AA   LBRC    // Å
+#define NO_DIAE RBRC    // ¨ (dead)
+#define NO_A    A       // A
+#define NO_S    S       // S
+#define NO_D    D       // D
+#define NO_F    F       // F
+#define NO_G    G       // G
+#define NO_H    H       // H
+#define NO_J    J       // J
+#define NO_K    K       // K
+#define NO_L    L       // L
+#define NO_OE   SEMI    // Ø
+#define NO_AE   SQT     // Æ
+#define NO_QUOT NUHS    // '
+#define NO_LABK NUBS    // <
+#define NO_Z    Z       // Z
+#define NO_X    X       // X
+#define NO_C    C       // C
+#define NO_V    V       // V
+#define NO_B    B       // B
+#define NO_N    N       // N
+#define NO_M    M       // M
+#define NO_COMM COMMA   // ,
+#define NO_DOT  PERIOD  // .
+#define NO_MINS SLASH   // -
